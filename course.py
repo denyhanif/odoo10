8dda8f4829d79304e2ce451cfbbebca7431907cb
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-07-25 22:48:19 +0700

    session

--- course.py
+++ course.py
@@ -10,3 +10,8 @@ class Course (models.Model):
         string="Responsible",
         required=True
     )
+    session_ids = fields.One2many(
+        comodel_name="academic.session",
+        string="nama session",
+        inverse_name="course_id"
+    )
--- session.py
+++ session.py
@@ -4,3 +4,10 @@ class Session(models.Model):
     _name = "academic.session"
 
     name = fields.Char("name", required= True, size = 100)
+
+    course_id = fields.Many2one( comodel_name="academic.course", string="Course")
+    instructor_id = fields.Many2one(comodel_name="res.partner", string="Instructor")
+    start_date = fields.Datetime("start date")
+    duration = fields.Integer("Duration")
+    seats = fields.Integer("seats")
+    active = fields.Boolean("Is Active", default= True)
